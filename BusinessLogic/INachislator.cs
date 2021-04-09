0640c0fb063b415c74a2529d924313f9c969b243
Леонид <leogri97@gmail.com>
2021-04-09 21:31:01 +0700

    Добавьте файлы проекта.

---
+++ BusinessLogic/INachislator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    /// <summary>
+    /// Интерфейс начисления з/п
+    /// </summary>
+    public interface INachislator
+    {
+        /// <summary>
+        /// Расчет з/п
+        /// </summary>
+        public void Calculate ()
+        {
+
+        }
+    }
+}
---
+++ CUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
